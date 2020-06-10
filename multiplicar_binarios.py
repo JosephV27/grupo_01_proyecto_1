5b16c5a428ec00b972f366eb1dec38b38cf889cc
Joseph <jvalencianom08@yahoo.com>
2020-06-10 15:30:57 -0600

    Cambiando entrada a numero decimal

--- multiplicar_binarios.py
+++ multiplicar_binarios.py
@@ -1,8 +1,11 @@
-def uint8_mult(bin1, bin2):
-    bin2 = bin2[::-1] # reverse the list to order the index
+def uint8_mult(num1, num2):
+    num1, num2 = bin(num1), bin(num2)
+    num2 = num2[::-1] # reverse the  binary list to order the index
     resultado = 0
-    for i in range(len(bin2)):
-        if bin2[i] == "1": # verify the active bits
-            SHL = int(bin1,2) << i #shift left
+    for i in range(len(num2)):
+        if num2[i] == "1": # verify the active bits
+            SHL = int(num1,2) << i #shift left
             resultado = resultado + SHL # sum the SHL results
     return bin(resultado)
+
+print(uint8_mult(8, 2))
